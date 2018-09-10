951ba7af2f37361bc3633c86d74abd952a1c0546
Rama <rmdhan95@gmail.com>
2018-09-10 17:37:59 +0700

    Shipping dn Checkout
    Shipping dan Check Out FIx With API RAJA ONGKIR

--- clean-admin/application/controllers/Shipping.php
+++ clean-admin/application/controllers/Shipping.php
@@ -11,6 +11,51 @@
 		}
 
 
+		function cek_city(){
+
+			$provinsi_id = $this->input->get('prov_id');
+			 
+			$curl = curl_init();
+			curl_setopt_array($curl, array(
+			  CURLOPT_URL => "http://api.rajaongkir.com/starter/city?province=$provinsi_id",
+			  CURLOPT_RETURNTRANSFER => true,
+			  CURLOPT_ENCODING => "",
+			  CURLOPT_MAXREDIRS => 10,
+			  CURLOPT_TIMEOUT => 30,
+			  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			  CURLOPT_CUSTOMREQUEST => "GET",
+			  CURLOPT_HTTPHEADER => array(
+			    "key: $this->api_key"
+			  ),
+			));
+			 
+			$response = curl_exec($curl);
+			$err = curl_error($curl);
+			 
+			curl_close($curl);
+			 
+			if ($err) {
+			  echo "cURL Error #:" . $err;
+			} else {
+			  //echo $response;
+			}
+			 
+			$data = json_decode($response, true);
+			echo "<option>-Pilih Kota/Kabupaten</option>";
+			for ($i=0; $i < count($data['rajaongkir']['results']); $i++) { 
+				if($data['rajaongkir']['results'][$i]['type']=='Kota'){
+					$type='Kota';
+				}else{
+					$type = 'Kab.';
+				}
+			    echo "
+                                
+
+			    <option value='".$data['rajaongkir']['results'][$i]['city_id']."'>".$type.' '.$data['rajaongkir']['results'][$i]['city_name']."</option>";
+			}
+		}
+
+
 		public function get_city(){
 			$curl = curl_init();
 
@@ -68,6 +113,39 @@
 		}
 
 
+		public function get_cost(){
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+			  CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+			  CURLOPT_RETURNTRANSFER => true,
+			  CURLOPT_ENCODING => "",
+			  CURLOPT_MAXREDIRS => 10,
+			  CURLOPT_TIMEOUT => 30,
+			  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			  CURLOPT_CUSTOMREQUEST => "POST",
+			  CURLOPT_POSTFIELDS => "origin=501&destination=114&weight=1700&courier=jne",
+			  CURLOPT_HTTPHEADER => array(
+			    "content-type: application/x-www-form-urlencoded",
+			    "key: $this->api_key"
+			  ),
+			));
+
+			$response = curl_exec($curl);
+			$err = curl_error($curl);
+
+			curl_close($curl);
+
+			if ($err) {
+			  echo "cURL Error #:" . $err;
+			} else {
+			  // echo $response;
+			  return json_decode($response);
+
+			}
+		}
+
+
 		public function tampil(){
 			$data['province'] = $this->get_province();
 			$this->load->view('shipping',$data);
@@ -77,8 +155,9 @@
 		function checkout(){
 		$data['province'] = $this->get_province();
 		$data['city'] = $this->get_city();
+		$data['cost'] = $this->get_cost();
 
-		// print_r($data['province']);
+		 // print_r($data['city']);
          
         $data['cart']=$this->cart->contents();
         $data['total']=number_format($this->cart->total());
